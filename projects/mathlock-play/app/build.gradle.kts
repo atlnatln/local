9587a67a6055b902b7581284cff465635f58ade4
Deploy Script <deploy@localhost>
2026-05-02 02:48:49 +0300

    chore(mathlock): bump version to 1.0.67 (67)

--- projects/mathlock-play/app/build.gradle.kts
+++ projects/mathlock-play/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.akn.mathlock.play"
         minSdk = 26
         targetSdk = 35
-        versionCode = 66
-        versionName = "1.0.66"
+        versionCode = 67
+        versionName = "1.0.67"
     }
 
     signingConfigs {
